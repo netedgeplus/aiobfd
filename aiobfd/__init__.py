c09b79dd45f71fda8f2aee3748b5a4d61b7870f0
Kris Lambrechts <kris@netedge.plus>
2017-09-19 14:16:03 +0200

    Ignore flake8 on import * for init

--- aiobfd/__init__.py
+++ aiobfd/__init__.py
@@ -1,7 +1,7 @@
 """aiobfd: Asynchronous BFD Daemon"""
 # pylint: disable=I0011,W0401
 
-from .control import *
-from .transport import *
+from .control import *  # noqa: F403
+from .transport import *  # noqa: F403
 
 __all__ = ['control', 'transport']
