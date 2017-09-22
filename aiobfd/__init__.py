35cdde5c00e89ecd3edabcc009abafdef684fc17
Kris Lambrechts <kris@netedge.plus>
2017-09-22 12:42:17 +0200

    Add all modules to package

--- aiobfd/__init__.py
+++ aiobfd/__init__.py
@@ -2,6 +2,8 @@
 # pylint: disable=I0011,W0401
 
 from .control import *  # noqa: F403
+from .packet import *  # noqa: F403
+from .session import *  # noqa: F403
 from .transport import *  # noqa: F403
 
-__all__ = ['control', 'transport']
+__all__ = ['control', 'packet', 'session', 'transport']
